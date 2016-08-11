31e9c6e5df3296c8fb28a5d3905e83e2b1e98dcd
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-11 23:22:12 +0300

    level23.lesson13.big01 - task06 done

--- src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
+++ src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
@@ -1,4 +1,27 @@
 package com.javarush.test.level23.lesson13.big01;
 
 public class SnakeSection {
+	private int x;
+	private int y;
+
+	public SnakeSection(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 }
