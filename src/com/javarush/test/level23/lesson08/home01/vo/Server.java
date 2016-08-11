ed276a79cdabf084ae71445457efbfd0b930b854
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-11 22:51:43 +0300

    level23.lesson08.home01 - done

---
+++ src/com/javarush/test/level23/lesson08/home01/vo/Server.java
@@ -0,0 +1,4 @@
+package com.javarush.test.level23.lesson08.home01.vo;
+
+public class Server extends NamedItem {
+}
---
+++ src/com/javarush/test/level23/lesson08/home01/vo/Subscription.java
@@ -0,0 +1,4 @@
+package com.javarush.test.level23.lesson08.home01.vo;
+
+public class Subscription extends NamedItem {
+}
