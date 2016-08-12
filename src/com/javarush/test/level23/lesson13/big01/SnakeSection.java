88804e5a27fd7b2285fde0dd60368ecfe76b3c62
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-12 12:18:43 +0300

    level23.lesson13.big01 - task13 done

--- src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
+++ src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
@@ -13,15 +13,7 @@ public class SnakeSection {
 		return x;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
 	public int getY() {
 		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
 	}
 }
