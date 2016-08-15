9932f728def4de64f17de5f6ff4daa2c6589c6a9
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-15 21:51:37 +0300

    level24.lesson14.big01 - task04 done

--- src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
+++ src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
@@ -1,8 +1,13 @@
 package com.javarush.test.level24.lesson14.big01;
 
+import java.util.ArrayList;
+
 public class Arcanoid {
 	private int width;
 	private int height;
+	private Ball ball;
+	private Stand stand;
+	private ArrayList<Brick> bricks;
 
 	public Arcanoid(int width, int height) {
 		this.width = width;
@@ -23,6 +28,30 @@ public class Arcanoid {
 
 	public void setHeight(int height) {
 		this.height = height;
+	}
+
+	public Ball getBall() {
+		return ball;
+	}
+
+	public void setBall(Ball ball) {
+		this.ball = ball;
+	}
+
+	public Stand getStand() {
+		return stand;
+	}
+
+	public void setStand(Stand stand) {
+		this.stand = stand;
+	}
+
+	public ArrayList<Brick> getBricks() {
+		return bricks;
+	}
+
+	public void setBricks(ArrayList<Brick> bricks) {
+		this.bricks = bricks;
 	}
 
 	public static void main(String[] args) {
