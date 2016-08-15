94763e6194e67412fb200634e05fd1ed5c657c46
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-15 21:49:21 +0300

    level24.lesson14.big01 - task03 done

--- src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
+++ src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
@@ -1,6 +1,30 @@
 package com.javarush.test.level24.lesson14.big01;
 
 public class Arcanoid {
+	private int width;
+	private int height;
+
+	public Arcanoid(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public static void main(String[] args) {
 
 	}
