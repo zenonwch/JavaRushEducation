8c560090a6c8dac7d115f386a574ac9fbce64709
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-16 20:02:51 +0300

    level24.lesson14.big01 - task13 done

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -1,8 +1,39 @@
 package com.javarush.test.level24.lesson14.big01;
 
 public class Ball extends BaseObject {
-	public Ball(double x, double y, double radius) {
-		super(x, y, radius);
+	private double speed;
+	private double direction;
+	private double dx;
+	private double dy;
+	private boolean isFrozen;
+
+	public Ball(double x, double y, double speed, double direction) {
+		super(x, y, 1);
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.direction = direction;
+		this.isFrozen = true;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public double getDirection() {
+		return direction;
+	}
+
+	public double getDx() {
+		return dx;
+	}
+
+	public double getDy() {
+		return dy;
+	}
+
+	public boolean isFrozen() {
+		return isFrozen;
 	}
 
 	@Override
