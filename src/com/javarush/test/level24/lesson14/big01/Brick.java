00dd2308fdcd54eaec9a9b6fbd0abba3a9eb334a
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-16 19:25:40 +0300

    level24.lesson14.big01 - task12 done

--- src/com/javarush/test/level24/lesson14/big01/Brick.java
+++ src/com/javarush/test/level24/lesson14/big01/Brick.java
@@ -1,8 +1,8 @@
 package com.javarush.test.level24.lesson14.big01;
 
 public class Brick extends BaseObject {
-	public Brick(double x, double y, double radius) {
-		super(x, y, radius);
+	public Brick(double x, double y) {
+		super(x, y, 3);
 	}
 
 	@Override
