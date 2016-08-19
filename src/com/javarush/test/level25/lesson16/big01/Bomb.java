18079a61d16aaf55fea1ec15fc32d96506154cb3
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-19 22:53:01 +0300

    level25.lesson16.big01- task12 done

--- src/com/javarush/test/level25/lesson16/big01/Bomb.java
+++ src/com/javarush/test/level25/lesson16/big01/Bomb.java
@@ -1,15 +1,26 @@
 package com.javarush.test.level25.lesson16.big01;
 
+/**
+ * Класс для бомбы.
+ */
 public class Bomb extends BaseObject {
 	public Bomb(double x, double y) {
 		super(x, y, 1);
 	}
 
-	public void move() {
-		y = y + 1;
-	}
-
+	/**
+	 * Отрисовываем себя на холсте.
+	 */
+	@Override
 	public void draw(Canvas canvas) {
 		canvas.setPoint(x, y, 'B');
+	}
+
+	/**
+	 * Двигаем себя вниз на один ход.
+	 */
+	@Override
+	public void move() {
+		y++;
 	}
 }
--- src/com/javarush/test/level25/lesson16/big01/Rocket.java
+++ src/com/javarush/test/level25/lesson16/big01/Rocket.java
@@ -1,7 +1,26 @@
 package com.javarush.test.level25.lesson16.big01;
 
+/**
+ * Класс для ракеты.
+ */
 public class Rocket extends BaseObject {
-	public Rocket(double x, double y, double radius) {
-		super(x, y, radius);
+	public Rocket(double x, double y) {
+		super(x, y, 1);
+	}
+
+	/**
+	 * Отрисовываем себя на холсте.
+	 */
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.setPoint(x, y, 'R');
+	}
+
+	/**
+	 * Двигаем себя вниз на один ход.
+	 */
+	@Override
+	public void move() {
+		y--;
 	}
 }
