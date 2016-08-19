eeb2a03acb235aea33372b60a5a340b142d6929c
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-19 22:42:58 +0300

    level25.lesson16.big01- task10 done

--- src/com/javarush/test/level25/lesson16/big01/Canvas.java
+++ src/com/javarush/test/level25/lesson16/big01/Canvas.java
@@ -39,4 +39,19 @@ public class Canvas {
 			}
 		}
 	}
+
+	public void clear() {
+		matrix = new char[width][height];
+	}
+
+	public void print() {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
+	}
 }
