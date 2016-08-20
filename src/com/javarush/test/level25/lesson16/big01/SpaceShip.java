6316f02005f173e9326ea18b43cd575a40d2f8cb
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-20 15:06:50 +0300

    level25.lesson16.big01- task14 done

--- src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
+++ src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
@@ -14,4 +14,19 @@ public class SpaceShip extends BaseObject {
 	public void moveRight() {
 		dx = 1;
 	}
+
+	public void move() {
+		if (x + dx - 3 < 0 || x + dx + 3 > Space.game.getWidth()) return;
+		x += dx;
+	}
+
+	public void draw() {
+	}
+
+	public void fire() {
+		Rocket leftR = new Rocket(x - 3, y);
+		Rocket rightR = new Rocket(x + 3, y);
+		Space.game.getRockets().add(leftR);
+		Space.game.getRockets().add(rightR);
+	}
 }
