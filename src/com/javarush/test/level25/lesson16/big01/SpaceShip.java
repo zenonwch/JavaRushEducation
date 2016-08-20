e306505dee51c0b40d2abc639bcf3890aa6e0831
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-20 21:56:57 +0300

    level25.lesson16.big01- task15 done

--- src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
+++ src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
@@ -1,32 +1,64 @@
 package com.javarush.test.level25.lesson16.big01;
 
+/**
+ * Класс для космического корабля
+ */
 public class SpaceShip extends BaseObject {
+	//картинка корабля для отрисовки
+	private static int[][] matrix = {
+			{0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 1, 0, 0},
+			{1, 0, 1, 0, 1},
+			{1, 1, 1, 1, 1},
+	};
+
+	//вектор движения (-1 влево,+1 вправо)
 	private double dx = 0;
 
 	public SpaceShip(int x, int y) {
 		super(x, y, 3);
 	}
 
+	/**
+	 * Устанавливаем вектор движения влево
+	 */
 	public void moveLeft() {
 		dx = -1;
 	}
 
+	/**
+	 * Устанавливаем вектор движения вправо
+	 */
 	public void moveRight() {
 		dx = 1;
 	}
 
+	/**
+	 * Метод рисует свой объект на "канвасе".
+	 */
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
+	}
+
+	/**
+	 * Двигаем себя на один ход.
+	 * Проверяем столкновение с границами.
+	 */
+	@Override
 	public void move() {
-		if (x + dx - 3 < 0 || x + dx + 3 > Space.game.getWidth()) return;
-		x += dx;
+		x = x + dx;
+
+		checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
 	}
 
-	public void draw() {
-	}
-
+	/**
+	 * Стреляем.
+	 * Создаем две ракеты: слева и справа от корабля.
+	 */
 	public void fire() {
-		Rocket leftR = new Rocket(x - 3, y);
-		Rocket rightR = new Rocket(x + 3, y);
-		Space.game.getRockets().add(leftR);
-		Space.game.getRockets().add(rightR);
+		Space.game.getRockets().add(new Rocket(x - 2, y));
+		Space.game.getRockets().add(new Rocket(x + 2, y));
 	}
 }
--- src/com/javarush/test/level25/lesson16/big01/Ufo.java
+++ src/com/javarush/test/level25/lesson16/big01/Ufo.java
@@ -1,7 +1,25 @@
 package com.javarush.test.level25.lesson16.big01;
 
 public class Ufo extends BaseObject {
-	public Ufo(double x, double y, double radius) {
-		super(x, y, radius);
+	private int moveCount = 0;
+
+	public Ufo(double x, double y) {
+		super(x, y, 3);
+	}
+
+	public void draw() {}
+
+	public void move() {
+		double dx = Math.random() * 2 - 1;
+		double dy = Math.random() * 2 - 1;
+		x += dx;
+		if (y + dy <= Space.game.getHeight() / 2) return;
+		y += dy;
+		moveCount++;
+		if (moveCount % 10 == 0) fire();
+	}
+
+	public void fire() {
+		Space.game.getBombs().add(new Bomb(x, y));
 	}
 }
