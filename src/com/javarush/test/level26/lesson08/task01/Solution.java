068d119eead702648b4abf57b935657d06231033
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-22 11:18:59 +0300

    level26.lesson08.task01- done

---
+++ src/com/javarush/test/level26/lesson08/task01/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level26.lesson08.task01;
+
+/* Вежливость - это искусственно созданное хорошее настроение.
+В классе Solution создайте public static класс IntegerHolder.
+IntegerHolder должен быть для типа int, быть нитебезопасным и изменяемым.
+В этом классе должны быть два public метода get и set
+*/
+public class Solution {
+	public static void main(String[] args) {
+	}
+
+	public static class IntegerHolder {
+		private int Int;
+
+		public synchronized int get() {
+			return Int;
+		}
+
+		public synchronized void set(int anInt) {
+			Int = anInt;
+		}
+	}
+}
