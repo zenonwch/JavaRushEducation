367dbd88e195da5daf8dfc90c986961400baa8e9
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-22 21:36:52 +0300

    level26.lesson15.big01- task02 done

--- src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -1,4 +1,21 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void writeMessage(String message) {
+		System.out.println(message);
+	}
+
+	public static String readString() {
+		String message = "";
+		try {
+			message = reader.readLine();
+		} catch (IOException e) {}
+		return message;
+	}
 }
