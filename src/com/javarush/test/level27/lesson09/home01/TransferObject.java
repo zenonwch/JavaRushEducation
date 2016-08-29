bba948312026c549c1fba355ec325616625be45a
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-08-29 16:04:23 +0300

    level27.lesson09.home01- done

---
+++ src/com/javarush/test/level27/lesson09/home01/TransferObject.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level27.lesson09.home01;
+
+public class TransferObject {
+	private int value;
+	protected volatile boolean isValuePresent = false; //use this variable
+
+	public synchronized int get() {
+		try {
+			while (!isValuePresent) this.wait();
+		}
+		catch (InterruptedException ignored) {
+		}
+		System.out.println("Got: " + value);
+		isValuePresent = false;
+		this.notify();
+		return value;
+	}
+
+	public synchronized void put(int value) {
+		try {
+			while (isValuePresent) this.wait();
+		} catch (InterruptedException ignored) {}
+		this.value = value;
+		System.out.println("Put: " + value);
+		isValuePresent = true;
+		this.notifyAll();
+	}
+}
