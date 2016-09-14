e1455d286fc409c2f6ee7c17837f7744b0f90954
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-09-14 10:29:16 +0300

    level27.lesson15.big01- task12 done

---
+++ src/com/javarush/test/level27/lesson15/big01/statistic/event/CookedOrderEventDataRow.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level27.lesson15.big01.statistic.event;
+
+import com.javarush.test.level27.lesson15.big01.kitchen.Dish;
+
+import java.util.Date;
+import java.util.List;
+
+public class CookedOrderEventDataRow implements EventDataRow {
+	private String tabletName;
+	private String cookName;
+	private int cookingTimeSeconds;
+	private List<Dish> cookingDishs;
+	private Date currentDate;
+
+	public CookedOrderEventDataRow(String tabletName, String cookName, int cookingTimeSeconds, List<Dish> cookingDishs) {
+		this.tabletName = tabletName;
+		this.cookName = cookName;
+		this.cookingTimeSeconds = cookingTimeSeconds;
+		this.cookingDishs = cookingDishs;
+		this.currentDate = new Date();
+	}
+}
---
+++ src/com/javarush/test/level27/lesson15/big01/statistic/event/NoAvailableVideoEventDataRow.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level27.lesson15.big01.statistic.event;
+
+import java.util.Date;
+
+public class NoAvailableVideoEventDataRow implements EventDataRow {
+	private int totalDuration;
+	private Date currentDate;
+
+	public NoAvailableVideoEventDataRow(int totalDuration) {
+		this.totalDuration = totalDuration;
+		this.currentDate = new Date();
+	}
+}
