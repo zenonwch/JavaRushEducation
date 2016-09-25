3f40d56e1011c5c0830768029a42dbcf3f7062c3
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-09-25 23:07:41 +0300

    level28.lesson15.big01- task03 done

--- src/com/javarush/test/level28/lesson15/big01/model/Provider.java
+++ src/com/javarush/test/level28/lesson15/big01/model/Provider.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level28.lesson15.big01.model;
 
+import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+
+import java.util.List;
+
 public class Provider {
 	private Strategy strategy;
 
@@ -9,5 +13,9 @@ public class Provider {
 
 	public Strategy getStrategy() {
 		return strategy;
+	}
+
+	public List<Vacancy> getJavaVacancies(String searchString) {
+		return null;
 	}
 }
