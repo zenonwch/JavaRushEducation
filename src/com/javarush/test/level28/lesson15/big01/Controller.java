09c31dad63dda3e394bda83c96f53c593fdd63c8
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-09-25 22:56:13 +0300

    level28.lesson15.big01- task02 done

---
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level28.lesson15.big01;
+
+import com.javarush.test.level28.lesson15.big01.model.Provider;
+
+import java.util.Arrays;
+
+public class Controller {
+	private Provider[] providers;
+
+	public Controller(Provider... providers) {
+		if (providers.length == 0) throw new IllegalArgumentException();
+		this.providers = providers;
+	}
+
+	@Override
+	public String toString() {
+		return "Controller{" +
+				"providers=" + Arrays.toString(providers) +
+				'}';
+	}
+}
