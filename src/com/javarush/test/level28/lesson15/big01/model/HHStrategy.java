be1122ce256937a257368a525d230fbff72fca2c
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-09-26 15:27:06 +0300

    level28.lesson15.big01- task04 done

--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -1,4 +1,5 @@
 package com.javarush.test.level28.lesson15.big01.model;
 
 public class HHStrategy implements Strategy {
+	private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
 }
