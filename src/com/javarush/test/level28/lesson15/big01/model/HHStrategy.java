ba4aedbe03c19710fad06596ee4ec6b42c54ea66
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-09-26 22:01:52 +0300

    level28.lesson15.big01- task06 done

--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -1,14 +1,24 @@
 package com.javarush.test.level28.lesson15.big01.model;
 
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
 	private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+	private static final String userAgent = "Mozilla/5.0 (jsoup)";
+	private static final int timeout = 5 * 1000;
 
 	@Override
 	public List<Vacancy> getVacancies(String searchString) {
+		Document doc;
+		try {
+			doc = Jsoup.connect(String.format(URL_FORMAT, searchString, 1)).userAgent(userAgent).timeout(timeout).get();
+		}
+		catch (IOException ignored) {}
 		return null;
 	}
 }
--- src/com/javarush/test/level28/lesson15/big01/vo/Vacancy.java
+++ src/com/javarush/test/level28/lesson15/big01/vo/Vacancy.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level28.lesson15.big01.vo;
 
+import java.util.Objects;
+
 public class Vacancy {
 	private String title;
 	private String salary;
@@ -54,5 +56,23 @@ public class Vacancy {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Vacancy vacancy = (Vacancy) o;
+		return Objects.equals(title, vacancy.title) &&
+				Objects.equals(salary, vacancy.salary) &&
+				Objects.equals(city, vacancy.city) &&
+				Objects.equals(companyName, vacancy.companyName) &&
+				Objects.equals(siteName, vacancy.siteName) &&
+				Objects.equals(url, vacancy.url);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(title, salary, city, companyName, siteName, url);
 	}
 }
