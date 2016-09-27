43ae6c4a9b9406e2b1ec338fe9ee6566d55324bf
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-09-27 18:31:56 +0300

    level28.lesson15.big01- task11 done

--- src/com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ src/com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,12 +1,6 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.HHStrategy;
-import com.javarush.test.level28.lesson15.big01.model.Provider;
-
 public class Aggregator {
 	public static void main(String[] args) {
-		Provider provider = new Provider(new HHStrategy());
-		Controller controller = new Controller(provider);
-		controller.scan();
 	}
 }
--- src/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -1,36 +1,17 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.Provider;
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.test.level28.lesson15.big01.model.Model;
 
 public class Controller {
-	private Provider[] providers;
+	private Model model;
 
-	public Controller(Provider... providers) {
-		if (providers.length == 0) throw new IllegalArgumentException();
-		this.providers = providers;
+	public Controller(Model model) {
+		if (model == null)
+			throw new IllegalArgumentException();
+		this.model = model;
 	}
 
-	@Override
-	public String toString() {
-		return "Controller{" +
-				"providers=" + Arrays.toString(providers) +
-				'}';
-	}
-
-	public void scan() {
-		List<Vacancy> vacancies = new ArrayList<>();
-		try {
-			for (Provider provider : providers)
-				for (Vacancy vacancy : provider.getJavaVacancies("Kiev"))
-					vacancies.add(vacancy);
-		}
-		catch (NullPointerException e) {}
-
-		System.out.println(vacancies.size());
+	public void onCitySelect(String cityName) {
+		model.selectCity(cityName);
 	}
 }
