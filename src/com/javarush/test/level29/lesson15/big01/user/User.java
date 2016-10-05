4d807275e6fa91c2f32ae11f573b817508ee8cfc
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-05 21:16:31 +0300

    level29.lesson15.big01- task14 done

--- src/com/javarush/test/level29/lesson15/big01/user/User.java
+++ src/com/javarush/test/level29/lesson15/big01/user/User.java
@@ -4,10 +4,9 @@ public class User {
 	private String name;
 	private String surname;
 	private int age;
+	private boolean man;
 
-	private String country;
-	private String city;
-	private House house;
+	private Address address;
 
 	private Work work;
 
@@ -41,24 +40,16 @@ public class User {
 		this.age = age;
 	}
 
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
 	public String getAddress() {
-		return country + " " + city + " " + house.house;
+		return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
+	}
+
+	public boolean isMan() {
+		return man;
+	}
+
+	public void setMan(boolean man) {
+		this.man = man;
 	}
 
 	public Work getWork() {
@@ -67,6 +58,10 @@ public class User {
 
 	public void setWork(Work work) {
 		this.work = work;
+	}
+
+	public String getBoss() {
+		return work.getBoss();
 	}
 
 	public void printInfo() {
--- src/com/javarush/test/level29/lesson15/big01/user/UserHelper.java
+++ src/com/javarush/test/level29/lesson15/big01/user/UserHelper.java
@@ -4,9 +4,6 @@ public class UserHelper {
 	private User userAnya = new User("Аня", "Смирнова", 10);
 	private User userRoma = new User("Рома", "Виноградов", 30);
 
-	private boolean isManAnya = false;
-	private boolean isManRoma = true;
-
 	public void printUsers() {
 		userAnya.printInfo();
 		userAnya.printAdditionalInfo();
@@ -26,7 +23,6 @@ public class UserHelper {
 	}
 
 	public String getBoosName(User user) {
-		Work work = user.getWork();
-		return work.getBoss();
+		return user.getBoss();
 	}
 }
