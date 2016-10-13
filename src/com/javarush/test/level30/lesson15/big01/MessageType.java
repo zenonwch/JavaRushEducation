dfb60587ecf6ca5ab30aa4a63a082c0d247568df
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-13 22:42:40 +0300

    level30.lesson15.big03- done

--- src/com/javarush/test/level30/lesson15/big01/MessageType.java
+++ src/com/javarush/test/level30/lesson15/big01/MessageType.java
@@ -1,4 +1,10 @@
 package com.javarush.test.level30.lesson15.big01;
 
 public enum MessageType {
+	NAME_REQUEST,           // запрос имени
+	USER_NAME,                  // имя пользователя
+	NAME_ACCEPTED,        // имя принято
+	TEXT,                               // текстовое сообщение
+	USER_ADDED,               // пользователь добавлен
+	USER_REMOVED          // пользователь удален
 }
