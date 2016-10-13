550425588de5ff44314ad5478ea144206e0bc05a
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-13 22:46:15 +0300

    level30.lesson15.big04- done

--- src/com/javarush/test/level30/lesson15/big01/Message.java
+++ src/com/javarush/test/level30/lesson15/big01/Message.java
@@ -1,4 +1,26 @@
 package com.javarush.test.level30.lesson15.big01;
 
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable {
+	private final MessageType type;
+	private final String data;
+
+	public Message(MessageType type, String data) {
+		this.type = type;
+		this.data = data;
+	}
+
+	public Message(MessageType type) {
+		this.type = type;
+		this.data = null;
+	}
+
+	public MessageType getType() {
+		return type;
+	}
+
+	public String getData() {
+		return data;
+	}
 }
