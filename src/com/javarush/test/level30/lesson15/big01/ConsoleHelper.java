1f78373abc10dc57de18a566cafb00451916610d
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-13 22:34:05 +0300

    level30.lesson15.big02- done

--- src/com/javarush/test/level30/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level30/lesson15/big01/ConsoleHelper.java
@@ -1,4 +1,41 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void writeMessage(String message) {
+		System.out.println(message);
+	}
+
+	public static String readString() {
+		String message;
+		while (true) {
+			try {
+				message = reader.readLine();
+				break;
+			}
+			catch (IOException e) {
+				writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+			}
+		}
+		return message;
+	}
+
+	public static int readInt() {
+		int number;
+		while (true) {
+			try {
+				number = Integer.parseInt(readString());
+				break;
+			}
+			catch (NumberFormatException e) {
+				writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+			}
+		}
+		return number;
+	}
 }
