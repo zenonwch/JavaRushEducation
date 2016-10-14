020b2410c02a3c719252ee4e4557c1c3b657b906
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-15 00:15:10 +0300

    level30.lesson15.big01- task16 done

--- src/com/javarush/test/level30/lesson15/big01/client/Client.java
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -89,5 +89,31 @@ public class Client {
 				Client.this.notify();
 			}
 		}
+
+		protected void clientHandshake() throws IOException, ClassNotFoundException {
+			while (true) {
+				Message message = Client.this.connection.receive();
+				if (message.getType() == MessageType.NAME_REQUEST)
+					connection.send(new Message(MessageType.USER_NAME, getUserName()));
+				else if (message.getType() == MessageType.NAME_ACCEPTED) {
+					notifyConnectionStatusChanged(true);
+					break;
+				}
+				else throw new IOException("Unexpected MessageType");
+			}
+		}
+
+		protected void clientMainLoop() throws IOException, ClassNotFoundException {
+			while (!Thread.currentThread().isInterrupted()) {
+				Message message = connection.receive();
+				if (message.getType() == MessageType.TEXT)
+					processIncomingMessage(message.getData());
+				else if (message.getType() == MessageType.USER_ADDED)
+					informAboutAddingNewUser(message.getData());
+				else if (message.getType() == MessageType.USER_REMOVED)
+					informAboutDeletingNewUser(message.getData());
+				else throw new IOException("Unexpected MessageType");
+			}
+		}
 	}
 }
