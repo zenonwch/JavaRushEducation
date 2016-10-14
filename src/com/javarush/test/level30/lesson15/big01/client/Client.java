bc42c7f9070bced22e2ba18284091d125282b24e
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-14 22:11:29 +0300

    level30.lesson15.big01- task12 done

---
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level30.lesson15.big01.client;
+
+import com.javarush.test.level30.lesson15.big01.Connection;
+
+public class Client {
+	protected Connection connection;
+	private volatile boolean clientConnected = false;
+
+	public class SocketThread extends Thread {
+
+	}
+}
