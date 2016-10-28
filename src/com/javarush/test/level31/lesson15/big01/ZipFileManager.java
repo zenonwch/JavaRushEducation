6a1c29016109924a810a5e06ed8badcf00daafd8
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-28 22:46:49 +0300

    level31.lesson15.big01- task14 done

--- src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -4,7 +4,6 @@ import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundExceptio
 import com.javarush.test.level31.lesson15.big01.exception.WrongZipFileException;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -54,6 +53,32 @@ public class ZipFileManager {
 		}
 	}
 
+	public List<FileProperties> getFilesList() throws Exception {
+		// Проверяем существует ли zip файл
+		if (!Files.isRegularFile(zipFile)) {
+			throw new WrongZipFileException();
+		}
+
+		List<FileProperties> files = new ArrayList<>();
+
+		try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+			ZipEntry zipEntry = zipInputStream.getNextEntry();
+
+			while (zipEntry != null) {
+				// Поля "размер" и "сжатый размер" не известны, пока элемент не будет прочитан
+				// Давайте вычитаем его в какой-то выходной поток
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				copyData(zipInputStream, baos);
+
+				FileProperties file = new FileProperties(zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize(), zipEntry.getMethod());
+				files.add(file);
+				zipEntry = zipInputStream.getNextEntry();
+			}
+		}
+
+		return files;
+	}
+
 	private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
 		Path fullPath = filePath.resolve(fileName);
 		try (InputStream inputStream = Files.newInputStream(fullPath)) {
@@ -73,19 +98,5 @@ public class ZipFileManager {
 		while ((len = in.read(buffer)) > 0) {
 			out.write(buffer, 0, len);
 		}
-	}
-
-	public List<FileProperties> getFilesList() throws Exception {
-		if (!Files.isRegularFile(zipFile)) throw new WrongZipFileException();
-		List<FileProperties> filesList = new ArrayList<>();
-		try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
-			ZipEntry entry;
-			while ((entry = zipInputStream.getNextEntry()) != null) {
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				copyData(zipInputStream, baos);
-				filesList.add(new FileProperties(entry.getName(), entry.getSize(), entry.getCompressedSize(), entry.getMethod()));
-			}
-		}
-		return filesList;
 	}
 }
--- src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
@@ -1,8 +1,20 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.FileProperties;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+
 public class ZipContentCommand extends ZipCommand {
 	@Override
 	public void execute() throws Exception {
+		ConsoleHelper.writeMessage("Просмотр содержимого архива.");
 
+		ZipFileManager zipFileManager = getZipFileManager();
+
+		ConsoleHelper.writeMessage("Содержимое архива:");
+		for(FileProperties fileProps : zipFileManager.getFilesList())
+			ConsoleHelper.writeMessage(fileProps.toString());
+
+		ConsoleHelper.writeMessage("Содержимое архива прочитано.");
 	}
 }
