5e556aee23950480c12fe830346e87e926ace124
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-30 00:00:37 +0300

    level32.lesson04.home01- done

---
+++ src/com/javarush/test/level32/lesson04/home01/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level32.lesson04.home01;
+
+import java.io.*;
+
+/* Читаем из потока
+Реализуйте логику метода getAllDataFromInputStream. Он должен вернуть StringWriter, содержащий все данные из переданного потока.
+Возвращаемый объект ни при каких условиях не должен быть null.
+Метод main не участвует в тестировании.
+*/
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+		System.out.println(writer.toString());
+	}
+
+	public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+		StringWriter sw = new StringWriter();
+
+		if (is != null) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+			while (br.ready()) {
+				sw.write(br.read());
+			}
+		}
+
+		return sw;
+	}
+}
