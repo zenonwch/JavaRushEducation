a4aede82824501f5e3300ccf0afe1c4fd83de914
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-30 20:51:01 +0200

    level32.lesson08.bonus01- done

---
+++ src/com/javarush/test/level32/lesson08/bonus01/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.test.level32.lesson08.bonus01;
+
+import com.javarush.test.level32.lesson08.bonus01.Big;
+import com.javarush.test.level32.lesson08.bonus01.Item;
+import com.javarush.test.level32.lesson08.bonus01.Small;
+
+import java.lang.reflect.Proxy;
+
+/* Дженерики для создания прокси-объекта
+В классе Solution создайте публичный метод getProxy
+1) Метод getProxy должен возвращать прокси для любого интерфейса, который наследуется от Item
+2) getProxy должен иметь два параметра. Первый - класс возвращаемого типа, второй - классы дополнительных интерфейсов.
+3) Используйте ItemInvocationHandler для создания прокси
+Метод main не участвует в тестировании
+*/
+public class Solution {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		test(solution.getProxy(Item.class));                        //true false false
+		test(solution.getProxy(Item.class, Small.class));           //true false true
+		test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
+		test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
+		test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
+	}
+
+	public Item getProxy(Class<?> loader, Class<?>... interfaces) {
+		ItemInvocationHandler iih = new ItemInvocationHandler();
+		int len = interfaces.length;
+		Class[] extendedI = new Class[len + 1];
+		System.arraycopy(interfaces, 0, extendedI, 0, len);
+		extendedI[len] = loader;
+		return (Item) Proxy.newProxyInstance(loader.getClassLoader(), extendedI, iih);
+	}
+
+	private static void test(Object proxy) {
+		boolean isItem = proxy instanceof Item;
+		boolean isBig = proxy instanceof Big;
+		boolean isSmall = proxy instanceof Small;
+
+		System.out.format("%b %b %b\n", isItem, isBig, isSmall);
+	}
+}
