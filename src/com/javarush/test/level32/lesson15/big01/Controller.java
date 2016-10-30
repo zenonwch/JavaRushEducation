64eeb043150f0e281fd2979a0966f60c43c0e123
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-10-30 21:55:09 +0200

    level32.lesson15.big01- task01 done

---
+++ src/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level32.lesson15.big01;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+	private View view;
+	private HTMLDocument document;
+	private File currentFile;
+
+	public Controller(View view) {
+		this.view = view;
+	}
+}
