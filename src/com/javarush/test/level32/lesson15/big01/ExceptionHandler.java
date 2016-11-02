baf1341e52f004cbb9c647b7cae168fa3eeb0929
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-11-02 22:31:45 +0200

    level32.lesson15.big01- task05 done

---
+++ src/com/javarush/test/level32/lesson15/big01/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level32.lesson15.big01;
+
+public class ExceptionHandler {
+	public static void log (Exception e) {
+		System.out.println(e.toString());
+	}
+}
---
+++ src/com/javarush/test/level32/lesson15/big01/listeners/TabbedPaneChangeListener.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level32.lesson15.big01.listeners;
+
+import com.javarush.test.level32.lesson15.big01.View;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class TabbedPaneChangeListener implements ChangeListener {
+	private View view;
+
+	public TabbedPaneChangeListener(View view) {
+		this.view = view;
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		view.selectedTabChanged();
+	}
+}
