fdcb0728a6f7f88d0d8eff3b34880299c37580fe
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-11-03 00:21:30 +0200

    level32.lesson15.big01- task09 done

--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -14,6 +14,15 @@ public class View extends JFrame implements ActionListener {
 	private JTextPane htmlTextPane = new JTextPane();
 	private JEditorPane plainTextPane = new JEditorPane();
 
+	public View() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e) {
+			ExceptionHandler.log(e);
+		}
+	}
+
 	public Controller getController() {
 		return controller;
 	}
@@ -30,6 +39,16 @@ public class View extends JFrame implements ActionListener {
 	}
 
 	public void initMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		MenuHelper.initFileMenu(this, menuBar);
+		MenuHelper.initEditMenu(this, menuBar);
+		MenuHelper.initStyleMenu(this, menuBar);
+		MenuHelper.initAlignMenu(this, menuBar);
+		MenuHelper.initColorMenu(this, menuBar);
+		MenuHelper.initFontMenu(this, menuBar);
+		MenuHelper.initHelpMenu(this, menuBar);
+
+		this.getContentPane().add(menuBar, BorderLayout.NORTH);
 	}
 
 	public void initEditor() {
