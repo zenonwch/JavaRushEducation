21a1b72a7c6f1de0adf3e5a48205fc411c675066
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-11-03 23:12:27 +0200

    level32.lesson15.big01- task12 done

--- src/com/javarush/test/level32/lesson15/big01/actions/SubscriptAction.java
+++ src/com/javarush/test/level32/lesson15/big01/actions/SubscriptAction.java
@@ -1,15 +1,25 @@
 package com.javarush.test.level32.lesson15.big01.actions;
 
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
 
 public class SubscriptAction extends StyledEditorKit.StyledTextAction {
 	public SubscriptAction() {
-		super(null);
+		super(StyleConstants.Subscript.toString());
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-
+	public void actionPerformed(ActionEvent actionEvent) {
+		JEditorPane editor = getEditor(actionEvent);
+		if (editor != null) {
+			MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+			SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+			StyleConstants.setSubscript(simpleAttributeSet, !StyleConstants.isSubscript(mutableAttributeSet));
+			setCharacterAttributes(editor, simpleAttributeSet, false);
+		}
 	}
 }
--- src/com/javarush/test/level32/lesson15/big01/actions/SuperscriptAction.java
+++ src/com/javarush/test/level32/lesson15/big01/actions/SuperscriptAction.java
@@ -1,15 +1,25 @@
 package com.javarush.test.level32.lesson15.big01.actions;
 
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
 
 public class SuperscriptAction extends StyledEditorKit.StyledTextAction {
 	public SuperscriptAction() {
-		super(null);
+		super(StyleConstants.Superscript.toString());
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-
+	public void actionPerformed(ActionEvent actionEvent) {
+		JEditorPane editor = getEditor(actionEvent);
+		if (editor != null) {
+			MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+			SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+			StyleConstants.setSuperscript(simpleAttributeSet, !StyleConstants.isSuperscript(mutableAttributeSet));
+			setCharacterAttributes(editor, simpleAttributeSet, false);
+		}
 	}
 }
