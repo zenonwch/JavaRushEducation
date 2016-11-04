93edb275c3cebf7ca4531fd6a4a7429533283cbf
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-11-04 22:09:41 +0200

    level32.lesson15.big01- task15 done

--- src/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level32.lesson15.big01;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -21,6 +22,14 @@ public class Controller {
 
 	public void exit() {
 		System.exit(0);
+	}
+
+	public void resetDocument() {
+		if (document != null)
+			document.removeUndoableEditListener(view.getUndoListener());
+		document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+		document.addUndoableEditListener(view.getUndoListener());
+		view.update();
 	}
 
 	public static void main(String[] args) {
