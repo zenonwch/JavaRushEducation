fdbcae3db1012e8d377f8262f387cbbc6c0902d3
Andrey Veshtard <andrey.veshtard@ctco.lv>
2016-11-04 22:34:39 +0200

    level32.lesson15.big01- task18 done

--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -84,7 +84,11 @@ public class View extends JFrame implements ActionListener {
 	}
 
 	public void selectedTabChanged() {
-
+		if (tabbedPane.getSelectedIndex() == 0)
+			controller.setPlainText(plainTextPane.getText());
+		else
+			plainTextPane.setText(controller.getPlainText());
+		resetUndo();
 	}
 
 	public boolean canUndo() {
